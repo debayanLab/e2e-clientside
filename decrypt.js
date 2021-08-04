0b8da6b3f1369bebf23b7af2d359a4c009661f8e
Akhil <akhilf007@gmail.com>
2021-08-04 23:35:36 +0530

    Kamakoti v2 Honest

--- decrypt.js
+++ decrypt.js
@@ -6,8 +6,7 @@ var privateKeyData = fs.readFileSync ("./keys/private.pem", "utf-8");
 var privateKey = new NodeRSA().importKey (privateKeyData);
 
 const ciphertext =
-`aebB+hyPNM6YqaEOGBsaCrfokYScVUfY9OntLCPomC0CL7mNCRokf/JxKw4h08qlOW68MRV2qyLywgV7MPIKvmq0kDcq2QWZCfawlasR03/PCLgj/5sOnVK/W+usyiRvKY3cyI+vXZGRfKbSHO+kp0Nmc4PqjfUUEPa+CRk0wwYYeW2rSLgDaf9DsS/wgZ0d+PobHrzAUuUQjpu92rRCDRIFml0ZZsyoK3yf7NiW5YCKw8ljgBaPQ4aovOA+UR7l0GXk5MpaPKSWQ1XdRkdbs8Z9vNuhTQkjOMnosTwFpZSkZ6KgV+RXdyQpYSdHZhd7RvW4Uvxp+skR/hzmLGnvlQ==`;
-
+`aXIBX/HVfaA26y3tKdJjzioGqqGGR2nNIABgJjYhlFb3CwIcX1bV25WeOsMk9d2NLQB4FHG7s8rXs9vtxCSbPq+ygGuJKJKL6vswpEo2kgraiqPjhDkL3kTdpYzvepQiTBZ9tDJywmKizr1aAPDoMauMx+TI1cgWNFvsFVK2IMCi35a4aYpBhjaUBKbOn/HeF0hwK+RA70pRnRh1q1xUk4poz6SfB59wyJt41bz5Zvlkm//pSh4NjaZL11JvhW+xBRDaLzZ3/D4f4BkyTDw8co7GaApLkxhhmqdAwrKYYHYSf5egauTeJi2QcPFbklvRoFx9+Yr0OUekj09J7uEG7Q==`
 const plaintext = privateKey.decrypt (ciphertext, "utf-8");
 
 console.log (plaintext);
