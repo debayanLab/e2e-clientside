5477e77cc7d45d7425bae55917c4ae45976ae09f
abhinav-256 <85794044+abhinav-256@users.noreply.github.com>
2021-09-09 18:38:59 +0545

    post

--- keys/test.js
+++ keys/test.js
@@ -1,15 +1,6 @@
-var sign=`aaEvXawa0aH0VOZpvTeD/WQaQ0Q7kB8kIdPDDcFugoJ5Pw8ujWkTaN6jYqkqtTM4uzVYJGODw1VSj0+PacFQFC+VKvrfmwTuPiwd3Ys0qVerFvwuQCSWhgpWJXxG0q7Ot5UwDCw0IBWdTmGZa0kU95lvghNh0Qmm3J8Lbk9zWau4tFChUUEkXkMYpT8+XtSsg5ElUL2WVPLldAxIn+LN7rAX2aCXFkdjKO3iw7xSFsmS4+ALtYuMdpZdilzJbhk0QiYMCqgGyxAyizwy4wFWMlz8qs28xbbUxLMlxX9sicArySfPrflHIAN5jbXdtivVEHVIsPQd1Scfaj+e69dz9A==`;
+var sign=`gVJMGDGHMSpW29TU9rsimhlUFMj/q6Pg5y9lNj8z8YjM+GOiFpmX5AyWJSUY2/bVjUjEjq85S2UO5HhV191b7ee6NeZlZzmn+E4+cvtW6Cm/Lk3V9DyAuoyZx6Mx7z+GFHscgr5Cix5BavuOarjZ1RUtlQa/dhEhEv0MK3vty4jYVNaDi+LNSWn7S0K0+39PoWeRf8xrEXlZp0rjV9hd1qakYY1xtmJurV2kyhcobU3L6nUHfIzeBsSE/8qXbN6o2jslAENJpy4gL3HmrlsxR9qB5plJzhJTUOvyReP1po/SeugTAWbZz4N/pMQnIoPpZsvRuCfj3xmgB7kq1E0/5A==`;
 
-var public =`-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAi3/dy/lapSSxXS6ZsAai
-/XyH7nWkWN49JCqwbtPJznx0Pi8+9JDy4exK0fXWGuAMzHAs4ahcieX/utwVXtsN
-3VHTer21ZiAe9qRogyIyOwwf+O2EaFngQYqXzgrFZmAIxrVdqOjwB8ra9R0+XV8O
-DMI214YZJOCScRiIjiGLPPJGSO0dkkGXCd9C2PGwyNeaLVkgk+Jkmgzlpsu67SFR
-EchGtdkWjU0mvtD/kGnkhWrmcJJPXwjH54vk890yVpUKxjUxtACq35tOcYDB5POM
-sxiwwX+5h9CcgWnBBr292YbzovdE04d0NvJBo/R1bzXVk/ni0u1QE+qf4zYgIyZT
-qQIDAQAB
------END PUBLIC KEY-----`;
-
+var public =`-----BEGIN PUBLIC KEY----- MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAi3/dy/lapSSxXS6ZsAai /XyH7nWkWN49JCqwbtPJznx0Pi8+9JDy4exK0fXWGuAMzHAs4ahcieX/utwVXtsN 3VHTer21ZiAe9qRogyIyOwwf+O2EaFngQYqXzgrFZmAIxrVdqOjwB8ra9R0+XV8O DMI214YZJOCScRiIjiGLPPJGSO0dkkGXCd9C2PGwyNeaLVkgk+Jkmgzlpsu67SFR EchGtdkWjU0mvtD/kGnkhWrmcJJPXwjH54vk890yVpUKxjUxtACq35tOcYDB5POM sxiwwX+5h9CcgWnBBr292YbzovdE04d0NvJBo/R1bzXVk/ni0u1QE+qf4zYgIyZT qQIDAQAB -----END PUBLIC KEY-----`;
 var NodeRSA = require ("node-rsa");
 
 var publickey =new NodeRSA();
